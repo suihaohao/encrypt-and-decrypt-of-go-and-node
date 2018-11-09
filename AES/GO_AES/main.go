d3b72232cc961b3b799f6bbef1d6d4d167b7399b
suihaohao <1255151021@qq.com>
2018-11-09 15:33:30 +0800

    Update main.go

--- AES/GO_AES/main.go
+++ AES/GO_AES/main.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	log "github.com/sirupsen/logrus"
+	"go_aes/decrypt"
+	"go_aes/encrypt"
+)
+
+type TestData struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Content  string `json:"content"`
+}
+
+func main() {
+  data := TestData{}
+  data.Name = "suihaohao"
+  data.Password = "1234565"
+  data.Content = "梅须逊雪三分白,雪却输梅一段香"
+  password := encrypt.makePwd(16)
+  encData := encrypt.AesEncrypt(password, data)
+  
+  log.println("encData:", encData)
+  
+  decData := decrypt(base64.StdEncoding.DecodeString(encData), []byte(password))
+  
+  log.println("decData:", decData)
+  
+}
