55cafa18c7d8a54702f8de3321789d1504708cb2
suihaohao <1255151021@qq.com>
2018-11-09 15:20:48 +0800

    Create decrypt.go

---
+++ AES/GO_AES/decrypt/decrypt.go
@@ -0,0 +1,30 @@
+package decrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/json"
+	"encrypt"
+	log "github.com/sirupsen/logrus"
+)
+
+func AesDecrypt(ciphertext []byte, password []byte, datalen int) (interface{}) {
+	key, iv := encrypt.ByteToKey(string(password), 16)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Printf("Error: NewCipher(%d bytes) = %s", len(password), err)
+	}
+	dec := cipher.NewCBCDecrypter(block, iv)
+	decrypted := make([]byte, len(ciphertext))
+	dec.CryptBlocks(decrypted, ciphertext)
+	decrypted = PKCS5UnPadding(decrypted)
+	var data map[string]interface{}
+	err = json.Unmarshal(decrypted, &data)
+	return data
+}
+
+func PKCS5UnPadding(plantText []byte) []byte {
+	length := len(plantText)
+	unpadding := int(plantText[length-1])
+	return plantText[:(length - unpadding)]
+}
