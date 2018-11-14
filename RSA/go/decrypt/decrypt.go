c342367fa5ef3991ce60b71e17c5c2b33dee5e99
suihaohao <1255151021@qq.com>
2018-11-14 11:53:17 +0800

    Create decrypt.go

---
+++ RSA/go/decrypt/decrypt.go
@@ -0,0 +1,39 @@
+package decrypt
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"go-crypto/encrypt"
+	"io/ioutil"
+)
+
+func RsaDecrypt(data string) (string, error) {
+	priKey, err := ioutil.ReadFile("../private.pem")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	block, _ := pem.Decode(priKey)
+	if block == nil {
+		return "", errors.New("private key error!")
+	}
+
+	pri, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+	priv := pri.(*rsa.PrivateKey)
+	cipherText, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		log.Error(err)
+	}
+	res, err := rsa.DecryptOAEP(sha1.New(), rand.Reader, pri, cipherText, nil)
+	return string(res), err
+}
